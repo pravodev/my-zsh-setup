2c729883ed3bae07c800bd953b3aafcb3d64a81e
Azam <pravodev@gmail.com>
2019-03-27 09:39:05 +0800

    init

---
+++ install.sh
@@ -0,0 +1,9 @@
+echo "======PRAVODEV ZSH SETUP======"
+echo "http://github.com/pravodev"
+echo ""
+echo ""
+echo "Installing..."
+cp * ~
+rm ~/install.sh
+echo "pravodev zsh setup installed"
+echo "done"
